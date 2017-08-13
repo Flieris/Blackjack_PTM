058a939c29d4e82bfa13d47bb18b15065e77a147
Sebastian Lenkiewicz <lenniuch@hotmail.com>
2017-08-13 23:53:57 +0200

    Changed from RelativeLayout to ConstraintLayout and added FloatingMenuButton to the project

--- app/src/main/java/com/example/lenni/blackjack_ptm/GameActivity.java
+++ app/src/main/java/com/example/lenni/blackjack_ptm/GameActivity.java
@@ -3,11 +3,15 @@ package com.example.lenni.blackjack_ptm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rjsv.floatingmenu.floatingmenubutton.FloatingMenuButton;
+
 public class GameActivity extends AppCompatActivity {
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_game);
+       FloatingMenuButton floatingButton = (FloatingMenuButton) findViewById(R.id.action_pick_button);
+       floatingButton.setAnchored(true);
    }
 }
